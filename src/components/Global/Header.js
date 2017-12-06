b04bf289b2354df33d3bb5676b026b7881e37c1f
Juan Parra <juanparra@ZCOHMBT100221-JuanParra.local>
2017-12-06 16:39:59 -0500

    Paso del componente header

---
+++ src/components/Global/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './images/logo.svg';
+import './css/Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <header className="Logo">
+          <img src={logo} alt="logo" />
+          <h1 className="App-title">Hello world React</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
