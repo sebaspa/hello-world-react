4a7b98ca21c2db95821be09249c0915bdb04636f
Juan Parra <juanparra@ZCOHMBT100221-JuanParra.local>
2017-12-06 16:41:10 -0500

    Paso del App a components

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+//Components
+import Header from './Global/Header';
+import Content from './Global/Content';
+import Footer from './Global/Footer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Content />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
