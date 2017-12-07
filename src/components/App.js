c6ac4ec3325a4e774de90614cd5933898aabe89e
Juan Parra <juanparra@ZCOHMBT100221-JuanParra.local>
2017-12-07 11:54:04 -0500

    Importar el data del menu y usar valores definidos para el componente footer y header

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+//Dependencies
 import React, { Component } from 'react';
 
 //Components
@@ -5,13 +6,16 @@ import Header from './Global/Header';
 import Content from './Global/Content';
 import Footer from './Global/Footer';
 
+//Data
+import items from '../data/menu';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header title="Test" items={items} />
         <Content />
-        <Footer />
+        <Footer copyright="&copy; Test 2017" />
       </div>
     );
   }
