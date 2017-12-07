9c7d0474bb44e047ac0a3477358e9bb27604cf07
Juan Parra <juanparra@ZCOHMBT100221-JuanParra.local>
2017-12-07 11:53:09 -0500

    Impor tar y usar protoypes y ajusteando el estilos del header

--- src/components/Global/Header.js
+++ src/components/Global/Header.js
@@ -1,14 +1,30 @@
+//Dependencies
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+//Assets
 import logo from './images/logo.svg';
 import './css/Header.css';
 
 class Header extends Component {
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    items: PropTypes.array.isRequired
+  };
+
   render() {
+
+    const {title, items} = this.props;
+    
     return (
       <div className="Header">
         <header className="Logo">
           <img src={logo} alt="logo" />
-          <h1 className="App-title">Hello world React</h1>
+          <h1 className="App-title">{title}</h1>
+          <div className="Menu">
+            {items && items.map((items, key) => <li key={key}>{items.title}</li>)}
+          </div>
         </header>
       </div>
     );
